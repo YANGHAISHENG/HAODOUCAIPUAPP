7d22ed8917099877db67cbc4d461a4c71bc28fb9
YANGHAISHENG <yanghaisheng88xy@163.com>
2016-08-19 19:18:00 +0800

    广场动态添加图片
    广场动态添加图片

--- HAODOUCAIPUAPP/Main/Square/View/DynamicGroupView/YHSDynamicGroupTableViewCell.m
+++ HAODOUCAIPUAPP/Main/Square/View/DynamicGroupView/YHSDynamicGroupTableViewCell.m
@@ -33,14 +33,11 @@ NSString * const CELL_IDENTIFIER_DYNAMIC_GROUP = @"YHSDynamicGroupTableViewCellI
 
 // 下部组件
 @property (nonnull, nonatomic, strong) UIView *downContainerView; // 下部容器组件
-@property (nonnull, nonatomic, strong) UILabel *sameFeatureLabel; // 概述按钮
 @property (nonnull, nonatomic, strong) UIImageView *commonImageView; // 评论图片
-@property (nonnull, nonatomic, strong) UILabel *commonTitleLabel; // 评论标题
-@property (nonnull, nonatomic, strong) UILabel *commonDescLabel; // 评论内容
 
-@property (nonnull, nonatomic, strong) UILabel *addressLabel; // 地址
-@property (nonnull, nonatomic, strong) MASConstraint* addressTopToCommonInfoView;
-@property (nonnull, nonatomic, strong) MASConstraint* addressTopToUserFavoriteContainerView;
+@property (nonnull, nonatomic, strong) UILabel *createTimeLabel; // 创建时间
+@property (nonnull, nonatomic, strong) MASConstraint* createTimeTopToCommonImageView;
+@property (nonnull, nonatomic, strong) MASConstraint* createTimeTopToDownContainerView;
 
 
 // 分割线
@@ -76,59 +73,90 @@ NSString * const CELL_IDENTIFIER_DYNAMIC_GROUP = @"YHSDynamicGroupTableViewCellI
     [self.rootContainerView addSubview:self.publicContainerView];
     
     // 上部容器组件
-    // 容器
     {
-        self.upContainerView =[[UIView alloc] init];
-        [self.upContainerView.layer setMasksToBounds:YES];
-        [self.publicContainerView addSubview:self.upContainerView];
+        // 容器
+        {
+            self.upContainerView =[[UIView alloc] init];
+            [self.upContainerView.layer setMasksToBounds:YES];
+            [self.publicContainerView addSubview:self.upContainerView];
+            
+            UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(pressUpViewArea:)];
+            tapGesture.numberOfTapsRequired = 1; // 设置点按次数，默认为1
+            tapGesture.numberOfTouchesRequired = 1; // 点按的手指数
+            [self.upContainerView addGestureRecognizer:tapGesture];
+        }
         
-        UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(pressUpViewArea:)];
-        tapGesture.numberOfTapsRequired = 1; // 设置点按次数，默认为1
-        tapGesture.numberOfTouchesRequired = 1; // 点按的手指数
-        [self.upContainerView addGestureRecognizer:tapGesture];
+        // 用户头像
+        self.avatarImageView = [UIImageView new];
+        [self.avatarImageView.layer setMasksToBounds:YES];
+        [self.avatarImageView setUserInteractionEnabled:YES];
+        [self.upContainerView addSubview:self.avatarImageView];
+        
+        // 用户名
+        self.userNameLabel = [UILabel new];
+        [self.userNameLabel setUserInteractionEnabled:YES];
+        [self.userNameLabel setTextColor:COLOR_NAVIGATION_BAR_TITLE_LIGHTGRAY];
+        [self.userNameLabel setFont:[UIFont systemFontOfSize:16.0]];
+        [self.userNameLabel setTextAlignment:NSTextAlignmentLeft];
+        [self.upContainerView addSubview:self.userNameLabel];
+        
+        // 介绍详情
+        self.introLabel = [UILabel new];
+        [self.introLabel setUserInteractionEnabled:YES];
+        [self.introLabel setTextColor:[UIColor colorWithRed:0.56 green:0.56 blue:0.57 alpha:1.00]];
+        [self.introLabel setFont:[UIFont systemFontOfSize:13.0]];
+        [self.introLabel setTextAlignment:NSTextAlignmentLeft];
+        [self.upContainerView addSubview:self.introLabel];
+        
+        // VIP标识
+        {
+            self.vipImageView = [UIImageView new];
+            [self.vipImageView.layer setMasksToBounds:YES];
+            [self.vipImageView setUserInteractionEnabled:YES];
+            [self.upContainerView addSubview:self.vipImageView];
+            
+            self.genderImageView = [UIImageView new];
+            [self.genderImageView.layer setMasksToBounds:YES];
+            [self.genderImageView setUserInteractionEnabled:YES];
+            [self.upContainerView addSubview:self.genderImageView];
+        }
+        
+        // 关注标识
+        self.relationImageView = [UIImageView new];
+        [self.relationImageView.layer setMasksToBounds:YES];
+        [self.relationImageView setUserInteractionEnabled:YES];
+        [self.upContainerView addSubview:self.relationImageView];
     }
     
-    // 用户头像
-    self.avatarImageView = [UIImageView new];
-    [self.avatarImageView.layer setMasksToBounds:YES];
-    [self.avatarImageView setUserInteractionEnabled:YES];
-    [self.upContainerView addSubview:self.avatarImageView];
     
-    // 用户名
-    self.userNameLabel = [UILabel new];
-    [self.userNameLabel setUserInteractionEnabled:YES];
-    [self.userNameLabel setTextColor:COLOR_NAVIGATION_BAR_TITLE_LIGHTGRAY];
-    [self.userNameLabel setFont:[UIFont systemFontOfSize:16.0]];
-    [self.userNameLabel setTextAlignment:NSTextAlignmentLeft];
-    [self.upContainerView addSubview:self.userNameLabel];
-    
-    // 介绍详情
-    self.introLabel = [UILabel new];
-    [self.introLabel setUserInteractionEnabled:YES];
-    [self.introLabel setTextColor:[UIColor colorWithRed:0.56 green:0.56 blue:0.57 alpha:1.00]];
-    [self.introLabel setFont:[UIFont systemFontOfSize:13.0]];
-    [self.introLabel setTextAlignment:NSTextAlignmentLeft];
-    [self.upContainerView addSubview:self.introLabel];
-    
-    // VIP标识
+    // 下部容器组件
     {
-        self.vipImageView = [UIImageView new];
-        [self.vipImageView.layer setMasksToBounds:YES];
-        [self.vipImageView setUserInteractionEnabled:YES];
-        [self.upContainerView addSubview:self.vipImageView];
+        // 容器
+        {
+            self.downContainerView =[[UIView alloc] init];
+            [self.downContainerView.layer setMasksToBounds:YES];
+            [self.publicContainerView addSubview:self.downContainerView];
+            
+            UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(pressDownViewArea:)];
+            tapGesture.numberOfTapsRequired = 1; // 设置点按次数，默认为1
+            tapGesture.numberOfTouchesRequired = 1; // 点按的手指数
+            [self.downContainerView addGestureRecognizer:tapGesture];
+        }
         
-        self.genderImageView = [UIImageView new];
-        [self.genderImageView.layer setMasksToBounds:YES];
-        [self.genderImageView setUserInteractionEnabled:YES];
-        [self.upContainerView addSubview:self.genderImageView];
+        // 评论图片
+        self.commonImageView = [UIImageView new];
+        [self.commonImageView.layer setMasksToBounds:YES];
+        [self.commonImageView setUserInteractionEnabled:YES];
+        [self.downContainerView addSubview:self.commonImageView];
+        
+        // 创建时间
+        self.createTimeLabel = [UILabel new];
+        [self.createTimeLabel setTextColor:[UIColor colorWithRed:0.56 green:0.56 blue:0.57 alpha:1.00]];
+        [self.createTimeLabel setFont:[UIFont systemFontOfSize:12.0]];
+        [self.createTimeLabel setTextAlignment:NSTextAlignmentLeft];
+        [self.downContainerView addSubview:self.createTimeLabel];
     }
-    
-    // 关注标识
-    self.relationImageView = [UIImageView new];
-    [self.relationImageView.layer setMasksToBounds:YES];
-    [self.relationImageView setUserInteractionEnabled:YES];
-    [self.upContainerView addSubview:self.relationImageView];
-    
+
     
     // 分割线
     self.separatorLineView =[[UIView alloc] init];
@@ -239,9 +267,45 @@ NSString * const CELL_IDENTIFIER_DYNAMIC_GROUP = @"YHSDynamicGroupTableViewCellI
         
     }
     
+    
+    // 下部容器
+    {
+        // 容器
+        [self.downContainerView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(weakSelf.upContainerView.mas_bottom);
+            make.left.equalTo(weakSelf.avatarImageView.mas_right).offset(margin);
+            make.right.equalTo(@(0.0));
+        }];
+        
+        // 评论图片
+        CGFloat commonImageSize = 80.0;
+        [self.commonImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(@(margin/2.0));
+            make.left.equalTo(@(0.0));
+            make.size.mas_equalTo(CGSizeMake(commonImageSize, commonImageSize));
+        }];
+        
+        // 创建时间
+        [self.createTimeLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(@(0.0));
+            make.right.equalTo(@(0.0));
+            
+            // 位置判断约束，因为不能同时存在，所以需要设置优先级
+            self.createTimeTopToCommonImageView = make.top.equalTo(weakSelf.commonImageView.mas_bottom).offset(margin).priorityHigh();
+            self.createTimeTopToDownContainerView = make.top.equalTo(weakSelf.downContainerView.mas_top).offset(margin/2.0).priorityLow();
+            [self.createTimeTopToCommonImageView deactivate];
+            [self.createTimeTopToDownContainerView deactivate];
+        }];
+        
+        // 约束完整性
+        [self.downContainerView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.bottom.equalTo(weakSelf.createTimeLabel.mas_bottom).offset(0.0);
+        }];
+    }
+
     // 分割线
     [self.separatorLineView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(weakSelf.upContainerView.mas_bottom).offset(margin);
+        make.top.equalTo(weakSelf.downContainerView.mas_bottom).offset(margin);
         make.left.equalTo(weakSelf.publicContainerView.mas_left).offset(0.0);
         make.right.equalTo(weakSelf.publicContainerView.mas_right).offset(0.0);
         make.height.equalTo(@(TABLE_FOOTER_SEPARATOR_VIEW_HEIGHT));
@@ -308,6 +372,28 @@ NSString * const CELL_IDENTIFIER_DYNAMIC_GROUP = @"YHSDynamicGroupTableViewCellI
     } // 上部
 
     
+    // 下部
+    {
+        // 评论图片
+        if (_model.data.Img.length > 0) {
+            [self.commonImageView sd_setImageWithURL:[NSURL URLWithString:_model.data.Img] placeholderImage:[UIImage imageNamed:PICTURE_PLACEHOLDER]];
+            
+            [self.createTimeTopToCommonImageView activate];
+            [self.createTimeTopToDownContainerView deactivate];
+        } else {
+            [self.commonImageView setImage:nil];
+            
+            [self.createTimeTopToCommonImageView deactivate];
+            [self.createTimeTopToDownContainerView activate];
+        }
+
+        // 创建日期
+        if (_model.data.CreateTime > 0) {
+            [self.createTimeLabel setText:_model.data.CreateTime];
+        }
+    }
+
+    
 }
 
 
@@ -321,6 +407,14 @@ NSString * const CELL_IDENTIFIER_DYNAMIC_GROUP = @"YHSDynamicGroupTableViewCellI
     }
 }
 
+
+// 点击下部评论详情容器
+- (void)pressDownViewArea:(UITapGestureRecognizer *)gesture
+{
+    if (self.delegate && [self.delegate respondsToSelector:@selector(didClickElementOfCellWithDynamicGroupModel:)]) {
+        [self.delegate didClickElementOfCellWithDynamicGroupModel:self.model];
+    }
+}
 
 
 @end
